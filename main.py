0111db5a47cec33745becc1f710019aadfa01fc9
Stanislaw <64831049+qu0ta@users.noreply.github.com>
2023-05-17 16:21:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+import logging
+
+app = Flask(__name__)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+
+@app.route("/")
+def main():
+	name = request.args.get('name') or "..."
+	message = request.args.get('message') or "..."
+	print(f"{name}! {message}")
+	return render_template('index.html', name=name, message=message)
+
+
+if __name__ == '__main__':
+	app.run(debug=False)
